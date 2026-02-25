45bc5e54f8b8de2cc0c4d38b6e8a72b127397c8e
Rafch1920 <rafik.chroud@inat.ucar.tn>
2026-02-25 12:18:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
+from app import init_db_command  # Importe la fonction
 
-app = create_app(os.getenv('FLASK_CONFIG', 'production'))
+app = create_app()
 
 @app.cli.command("init-db")
 def init_db():
-    db.create_all()
-    print("Base de données créée!")
+    """Initialise la base de données avec données exemples"""
+    init_db_command()
 
 if __name__ == '__main__':
-    # Utilise le port de Render ou 5000 par défaut
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
