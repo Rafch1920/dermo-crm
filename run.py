65e816a026b1d45cb5169bf85cf5c6f35de1becc
Rafch1920 <rafik.chroud@inat.ucar.tn>
2026-02-25 12:59:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
-from app import init_db_command  # Importe la fonction
 
 app = create_app()
 
 @app.cli.command("init-db")
 def init_db():
     """Initialise la base de données avec données exemples"""
-    init_db_command()
+    from app import _create_initial_data
+    
+    with app.app_context():
+        db.create_all()
+        print("✅ Tables créées")
+        _create_initial_data()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
