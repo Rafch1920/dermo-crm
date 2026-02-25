d615ee82c0522fb63d8a55cd31eb479eab4e3201
Rafch1920 <rafik.chroud@inat.ucar.tn>
2026-02-25 12:14:57 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
-from app.models import User, Referent, Product, Campaign, Pharmacy
 
-# CRUCIAL : Définir app au niveau global pour Gunicorn
 app = create_app(os.getenv('FLASK_CONFIG', 'production'))
 
 @app.cli.command("init-db")
 def init_db():
-    """Initialise la base de données"""
     db.create_all()
-    print("✅ Base de données créée!")
+    print("Base de données créée!")
 
 if __name__ == '__main__':
-    app.run()
+    # Utilise le port de Render ou 5000 par défaut
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
